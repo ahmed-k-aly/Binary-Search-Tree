905c1331d5aa15d25460339975cefc3d34903af3
ahmed-k-aly <ahmedkaly1101@gmail.com>
2022-06-01 18:38:27 -0400

    Create Remove method.

--- BST.js
+++ BST.js
@@ -14,7 +14,7 @@ export default class BinaryTree{
 
     /**
      * Adds element to the tree if it is not already.
-     * @param {*} _value 
+     * @param {*} _value value to add to tree
      * @return {boolean} returns true if the element is added successfully, false otherwise
      */
     add(_value){
@@ -22,11 +22,46 @@ export default class BinaryTree{
         let parentNode = find(_value);
         if (parentNode == null) return false;
         let toAdd = Node(_value);
-        isRightChild = compareNodes(toAdd, parentNode);
-        parentNode.right = toAdd;
+
+        // adds the element to the tree
+        let isRightChild = compareNodes(toAdd, parentNode);
+        if (isRightChild) parentNode.right = toAdd;
+        else parentNode.left = toAdd;
+        
+        this.#size++;
+        
         return true;
-        // else, add element to tree
     }
+
+    /**
+     * Removes an element from the tree if it exists
+     * @param {*} _value value to remove from tree 
+     * @return {boolean} returns true if the element is removed successfully, false otherwise
+     */
+    remove(_value){
+        // if element is not in the tree, return false
+        let toRemove = find(_value);
+        if (toRemove == null) return false;
+        // element is in the tree
+        let parentNode = toRemove.parent;
+        let newChild = deleteNode(toRemove)
+        this.#size--;
+        // find if removed node is a right child, left child, or a root
+        if (parentNode == null){
+            // if removed node is a root.
+            this.#root = newChild;
+            return true;
+        }
+        
+        let isRightChild = compareNodes(toRemove, parentNode);
+        if (isRightChild) parentNode.right = newChild;
+        else parentNode.left = newChild;
+        
+        return true;
+
+    }
+
+
 
     get size(){
         return this.#size;
