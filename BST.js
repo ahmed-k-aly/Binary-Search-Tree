c43acf62d53b74d11bfb53b08d1854985647f41c
ahmed-k-aly <ahmedkaly1101@gmail.com>
2022-06-01 18:13:10 -0400

    Create Add method

--- BST.js
+++ BST.js
@@ -1,14 +1,34 @@
 class Node{
-    next = null;
+    left = null;
+    right = null;
     value = null;
     parent = null;
     constructor(value) {
         this.value = value;
-        this.next = null;
     }
 }
 
 export default class BinaryTree{
     #root;
     #size;
+
+    /**
+     * Adds element to the tree if it is not already.
+     * @param {*} _value 
+     * @return {boolean} returns true if the element is added successfully, false otherwise
+     */
+    add(_value){
+        // if element already exists, return false
+        let parentNode = find(_value);
+        if (parentNode == null) return false;
+        let toAdd = Node(_value);
+        isRightChild = compareNodes(toAdd, parentNode);
+        parentNode.right = toAdd;
+        return true;
+        // else, add element to tree
+    }
+
+    get size(){
+        return this.#size;
+    }
 }
