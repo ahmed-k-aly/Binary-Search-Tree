1840d21c84fcd5091082551a8c8a9baa47aae97a
ahmed-k-aly <ahmedkaly1101@gmail.com>
2022-06-01 23:00:45 -0400

    testing outline.

---
+++ tester.js
@@ -0,0 +1,20 @@
+addTester();
+removeTester();
+findTester();
+sizeTester();
+
+function addTester(){
+    return true;
+}
+
+function removeTester(){
+    return true;
+}
+
+function findTester(){
+    return true;
+}
+
+function sizeTester(){
+    return true;
+}
