4a6a438839b77cce7713472c97206c02d2b8ca95
ahmed-k-aly <ahmedkaly1101@gmail.com>
2022-06-01 22:14:41 -0400

    Create Find method.

--- BST.js
+++ BST.js
@@ -18,7 +18,7 @@ export default class BinaryTree{
      * @return {boolean} returns true if the element is added successfully, false otherwise
      */
     add(_value){
-        // if element already exists, return false
+        // find element or where it is supposed to be
         let parentNode = find(_value);
         if (parentNode == null) return false;
         let toAdd = Node(_value);
@@ -41,7 +41,7 @@ export default class BinaryTree{
     remove(_value){
         // if element is not in the tree, return false
         let toRemove = find(_value);
-        if (toRemove == null) return false;
+        if (toRemove.value != _value) return false;
         // element is in the tree
         let parentNode = toRemove.parent;
         let newChild = deleteNode(toRemove)
@@ -62,6 +62,49 @@ export default class BinaryTree{
     }
 
 
+    /**
+     * searches for a value in the tree. 
+     * @param {*} _value value to be found in the tree.
+     * @return {Node} returns the node containing that value if found
+     *                else, returns its parent if it would exist.
+     */
+    find(_value){
+        return findHelper(this.#root, _value);
+    }
+
+    /**
+     * 
+     * @param {Node} root the root node from the perspective of the method
+     * @param {*} value the value to find
+     * @return {Node} returns the node containing that value if found
+*                else, returns its parent if it would exist. 
+     */
+    findHelper(root, value){
+        if (root.value === value){
+            return root;
+        }
+        if (value < root.value){
+            // go left
+            // if we can't go left anymore, this is where the node would be
+
+            if (root.left == null) return root;
+            return findHelper(root.left, value);
+        }
+        // go right
+        // if we can't go right anymore, this is where the node would be
+        if (root.right == null) return root;
+        return findHelper(root.right, value);
+    }
+
+
+
+
+    /**
+     * @return {String} an inorder string representation of the tree
+     */
+     toString(){
+        return "";
+    }
 
     get size(){
         return this.#size;
